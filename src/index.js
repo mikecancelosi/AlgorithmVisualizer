2353b789e436f887dbc9c9b76eec42bbf91db171
Mike Cancelosi <mike.cancelosi@gmail.com>
2020-05-12 20:21:56 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Slider from '@material-ui/core/Slider';
+
+function DrawBox(props){
+  return(
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+    );  
+}
+
+
+class Row extends React.Component{
+  constructor(props){
+    super(props);
+    this.state= {
+      blocks: props.blocks
+    };
+  }
+
+  render(){
+    return(
+    <button></button>
+    );
+  }
+}
+
+
+class Visualizer extends React.Component { 
+
+  constructor(props){
+    super(props);
+    this.state={
+      nCount: 3,
+    };
+  }
+
+  handleChange = (event, value) => this.setState({ value });  
+  handleDragStop = () => this.props.update(this.state.nCount);
+
+  render(){
+    return(
+        <NSlider value= {this.state.nCount} onChange = {this.handleChange} onDragStop={this.handleDragStop}/>
+    );
+  };
+}
+
+class NSlider extends React.Component {
+  render() { 
+    return(
+      <div class = "nSlider" >   
+      <Slider 
+        defaultValue = {15}
+        min = {3}
+        max = {1000}
+        valueLabelDisplay = "auto"
+        step ={1}
+        />
+      </div>
+      );
+  };
+}
+
+class Navigation extends React.Component{
+  render(){
+    return (
+        <div className = "container center">
+          <div className = "menu">
+            <div class = "menu-left">
+              <ul class = "menu-list">
+                <li class = "menu-list-item"><a class="menu-link menu-link--active" href="#">Sorting</a></li>
+                <li class = "menu-list-item"><a class="menu-link" href="#">Pathfinding</a></li>                
+              </ul>
+            </div>
+          </div>
+        </div>
+    );
+  };
+}
+
+class App extends React.Component{
+  
+  render(){
+    return(      
+      <div className = "container">
+        <Navigation/>
+        <Visualizer/>
+      </div>
+    );
+  };
+}
+
+// ========================================
+ReactDOM.render(<App/>,document.getElementById("root"));
